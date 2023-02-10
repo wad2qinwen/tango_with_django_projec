d607026f7781021e3b9268aaf5ebcc02bca578ad
Qinwen <2721817y@student.gla.ac.uk>
2023-02-10 14:24:15 +0000

    'chapter3'

--- rango/views.py
+++ rango/views.py
@@ -3,14 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     
-    #construct a dictionary to pass to template as its content
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    return render(request, 'rango/index.html', context = context_dict)
+    return HttpResponse('Rango says hey there partner! <a href=\'/rango/about/\'>About</a>')
 
 def about(request):
      
-    context_dict = {'about_message': 'here is the about page.',
-                    'information': 'This tutorial has been put together by Qinwen Yan',
-                    }
-    return render(request, 'rango/about.html', context = context_dict)
+     return HttpResponse('Rango says here is the about page. <a href=\'/rango/\'>Index</a>')
  
